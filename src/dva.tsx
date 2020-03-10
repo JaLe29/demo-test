aaf5c395d5b8855cf9ab5e6362a5da8d3a3bc370
Jakub Löffelmann <jakubloffelmann@gmail.com>
2020-03-10 22:59:32 +0100

    x

---
+++ src/dva.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Dva = () => {
+	return (
+		<div>
+			DVA
+		</div>
+	)
+}
+
+export default Dva
--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,37 @@
 
 import * as React from 'react'
 
-import styles from './styles.css'
+// import styles from './styles.css'
 
 export type Props = { text: string }
+import Dva from './dva'
 
-export default class ExampleComponent extends React.Component<Props> {
+// export default class ExampleComponent extends React.Component<Props> {
 
-	render() {
-		const {
-			text
-		} = this.props
+// 	render() {
+// 		const {
+// 			text
+// 		} = this.props
 
-		return (
-			<div className={styles.test}>
-				xExample Component: {text}
-			</div>
-		)
-	}
+// 		return (
+// 			<div className={styles.test}>
+// 				xExample Component: {text}
+// 			</div>
+// 		)
+// 	}
+// }
+
+const Home = () => {
+	const [c, sc] = React.useState(0)
+
+	return (
+		<div>
+			TEST
+			{c}
+			<button onClick={() => sc(c + 1)}>CLICK ME</button>
+			<Dva />
+		</div>
+	)
 }
+
+export default Home
