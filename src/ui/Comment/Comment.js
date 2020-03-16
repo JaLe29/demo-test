d8703e1b2678841004220545d7051ef697e44712
Jakub Löffelmann <jakubloffelmann@gmail.com>
2020-03-16 19:07:38 +0100

    x

--- src/ui/Comment/Comment.js
+++ src/ui/Comment/Comment.js
@@ -1,18 +1,23 @@
-import React, { useCallback } from 'react'
+import React, {useCallback} from 'react'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const css = {}
 
-const Comment = ({ text, createdAt }) => {
-  const getDistance = useCallback(() => new Date(createdAt).toJSON(), [createdAt])
+const Comment = ({text, createdAt}) => {
+	const getDistance = useCallback(
+		() => formatDistanceToNow(new Date(createdAt), {addSuffix: true}),
+		[createdAt]
+	)
 
-  return (
-    <div className={css.comment}>
-      <div className={css.hedaer}>
-        <span className={css.name}>FEJKNAME</span>
-        <span className={css.date}>{getDistance()}</span>
-      </div>
-      <p className={css.text}>{text}</p>
-    </div>
-  )
+	return (
+		<div className={css.comment}>
+			<div className={css.hedaer}>
+				<span className={css.name}>FEJKNAME</span>
+				<span className={css.date}>{getDistance()}</span>
+			</div>
+			<p className={css.text}>{text}</p>
+		</div>
+	)
 }
 
 export default Comment
--- src/ui/TextArea/TextArea.js
+++ src/ui/TextArea/TextArea.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
-// import css from './text-area.scss'
-const css = {}
+import React, {useState, useEffect} from 'react'
+import css from './textArea.module.scss'
 const TIMEOUT = 2000
+
 
 // type Props = {
 // 	inputState: string,
@@ -9,63 +9,65 @@ const TIMEOUT = 2000
 // 	setInputState: (value: string) => void,
 // }
 
-const TextArea = ({ onSubmit, setInputState, inputState }) => {
-  const [isFocused, setFocused] = useState(false)
-  const [value, setValue] = useState('')
-  let blockEnter = false
+const TextArea = ({onSubmit, setInputState, inputState}) => {
+	const [isFocused, setFocused] = useState(false)
+	const [value, setValue] = useState('')
+	let blockEnter = false
 
-  useEffect(() => {
-    let timer
-    if (inputState === 'done') {
-      timer = setTimeout(() => {
-        setFocused(false)
-        setInputState('ready')
-      }, TIMEOUT)
-    }
+	useEffect(() => {
+		let timer
+		if (inputState === 'done') {
+			timer = setTimeout(() => {
+				setFocused(false)
+				setInputState('ready')
+			}, TIMEOUT)
+		}
 
-    return () => {
-      if (timer) clearTimeout(timer)
-    }
-  }, [inputState, setInputState])
+		return () => {
+			if (timer) clearTimeout(timer)
+		}
+	}, [inputState, setInputState])
 
-  const handleFocus = () => setFocused(true)
+	const handleFocus = () => setFocused(true)
 
-  const handleChange = e => {
-    if (e.keyCode === 13 && e.shiftKey === false) {
-      blockEnter = true
+	const handleChange = e => {
+		if (e.keyCode === 13 && e.shiftKey === false) {
+			blockEnter = true
 
-      const transformed = value.trim()
-      if (transformed.length === 0) return
-      onSubmit(transformed)
-      setValue('')
+			const transformed = value.trim()
+			if (transformed.length === 0) return
+			onSubmit(transformed)
+			setValue('')
 
-      return
-    }
-    if (blockEnter) {
-      blockEnter = false
-      return
-    }
-    const text = e.target.value || ''
-    setValue(text)
-  }
+			return
+		}
+		if (blockEnter) {
+			blockEnter = false
+			return
+		}
+		const text = e.target.value || ''
+		setValue(text)
+	}
 
-  const cls = [css.textArea]
-  if (!isFocused) cls.push(css.notFocused)
-  else cls.push(css.normal)
+	const cls = [css.textArea]
+	if (!isFocused) cls.push(css.notFocused)
+	else cls.push(css.normal)
 
-  return ['sending', 'done'].includes(inputState) ? (
-    'DONE'
-  ) : (
-    <textarea
-      value={value}
-      onChange={handleChange}
-      onKeyDown={handleChange}
-      onFocus={handleFocus}
-      onBlur={() => setFocused(false)}
-      className={cls.join(' ')}
-      placeholder="Join the discussion..."
-    />
-  )
+	return ['sending', 'done'].includes(inputState)
+		? (
+			'DONE'
+		)
+		: (
+			<textarea
+				value={value}
+				onChange={handleChange}
+				onKeyDown={handleChange}
+				onFocus={handleFocus}
+				onBlur={() => setFocused(false)}
+				className={cls.join(' ')}
+				placeholder="Join the discussion..."
+			/>
+		)
 }
 
 export default React.memo(TextArea)
